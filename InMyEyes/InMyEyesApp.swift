56b310126219d2214ef86ad29b482167bc109219
Manuele Esposito <mannyes1996@gmail.com>
2024-12-09 11:53:48 +0100

    Initial Commit

---
+++ InMyEyes/InMyEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  InMyEyesApp.swift
+//  InMyEyes
+//
+//  Created by Manuele Esposito on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct InMyEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
